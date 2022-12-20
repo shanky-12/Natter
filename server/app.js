c25d581d86e00fc7657facca4613b0c969e8332e
Kyle Henderson <81394947+tester242@users.noreply.github.com>
2022-12-20 14:28:23 -0500

    Update app.js
    got rid of unneeded function

--- server/app.js
+++ server/app.js
@@ -28,14 +28,6 @@ io.on('connection', (socket) => {
   });
 });
 
-async function addMessage(name,message,socketID){
-  var newMessage={
-    name:name,
-    message,message,
-    time: Date.now()
-  };
-  return newMessage;
-}
 
 http.listen(3000, function() {
   console.log(`listening on *:${3000}`);
